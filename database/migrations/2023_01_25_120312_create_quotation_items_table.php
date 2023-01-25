aedfd2249e3db8c1b1c2cb0d6b192c031ef9090e
reddhilario <84271181+reddhilario@users.noreply.github.com>
2023-01-25 21:56:30 +0800

    created migration files for quotation

---
+++ database/migrations/2023_01_25_120312_create_quotation_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuotationItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotation_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('description');
+            $table->integer('qty');
+            $table->float('unit_price');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotation_items');
+    }
+}
